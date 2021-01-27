40f27450f90aab2703f1838fff3d0cb3c997ef27
drthvdr <jaredrodr2@gmail.com>
2021-01-26 20:55:17 -0500

    Index, mail modal. Initial UI design.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,10 +7,15 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-require("bootstrap")
-require("packs/custom")
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
+import "bootstrap"
+require('packs/custom')
+
+document.addEventListener("turbolinks:load", () => {
+    $('[data-toggle="tooltip"]').tooltip()
+    $('[data-toggle="popover"]').popover()
+})
--- app/javascript/packs/custom.js
+++ app/javascript/packs/custom.js
@@ -1,6 +1,1 @@
 console.log("custom js file loaded");
-
-function notify() {
-  alert( "clicked" );
-}
-$( "#fuck" ).on( "click", notify );
